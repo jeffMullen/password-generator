79f737912a9386ac71098875e36b0c0ccf52a99d
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-06-18 19:07:16 -0400

    validating lowercase, uppercase, number, and special character options; concatenating all associated arrays into one array - passwordCredentials

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,38 +28,38 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // // Prompt String Variables
-// var lowercase = "Would you like to use lowercase letters?";
-// var uppercase = "Would you like to use uppercase letters?";
-// var numQuestion = "Would you like to use numbers?"
-// var specialQuestion = "Would you like to use special characters?";
+var lowercase = "Would you like to use lowercase letters?";
+var uppercase = "Would you like to use uppercase letters?";
+var numQuestion = "Would you like to use numbers?"
+var specialQuestion = "Would you like to use special characters?";
 
 // // Random letter generator
-// var alpha = "abcdefghijklmnopqrstuvwxyz";
-// var alphaArray = alpha.split("");
+var alpha = "abcdefghijklmnopqrstuvwxyz";
+var alphaArray = alpha.split("");
 
 // var randomLetter = alphaArray[Math.floor(Math.random() * alphaArray.length)];
 
 // console.log(randomLetter);
 
 // // Random Uppercase letter generator
-// var alphaUppercase = alpha.toUpperCase();
-// var alphaArrayUppercase = alphaUppercase.split("");
+var alphaUppercase = alpha.toUpperCase();
+var alphaArrayUppercase = alphaUppercase.split("");
 
 // var randomUpperCase = alphaArrayUppercase[Math.floor(Math.random() * alphaArrayUppercase.length)];
 
 // console.log(randomUpperCase);
 
 // // Random number generator
-// var num = "0123456789";
-// var numArray = num.split("");
+var num = "0123456789";
+var numArray = num.split("");
 
 // var randomNum = numArray[Math.floor(Math.random() * numArray.length)];
 
 // console.log(randomNum);
 
 // // Random Special Character Generator
-// var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}";
-// var specialArray = special.split("");
+var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}";
+var specialArray = special.split("");
 
 // var randomSpecial = specialArray[Math.floor(Math.random() * specialArray.length)];
 
@@ -68,72 +68,76 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // // Object to store validation data
-// var passwordCredentials = {};
+var passwordCredentials = [];
 
 
 // Ask User for Criteria
 function generatePassword() {
   var numCharacters = window.prompt("Choose a password length between 8 and 128 characters.");
 
-  // while (numCharacters !== parseInt(numCharacters)) {
-  //   alert("Entry must be a number");
-  //   numCharacters = window.prompt("Choose a password length between 8 and 128 characters.")
-  // }
-
-  // while ((numCharacters < 8) || (numCharacters > 128)) {
-  //   if (numCharacters < 8 || numCharacters > 128) {
-  //     numCharacters = parseInt(window.prompt("Invalid Entry" + "\nPassword must be at least 8 characters, and no more than 128 characters."))
-  //   }
-  // }
   // passwordCredentials.characters = numCharacters;
   console.log("numCharacters ", numCharacters);
   console.log(typeof parseInt(numCharacters));
 
-
-
-
-
+  // check that its a number
   while (isNaN(parseInt(numCharacters))) {
     alert("Entry must be a number");
     numCharacters = window.prompt("Choose a password length between 8 and 128 characters.");
   }
 
-
+  //check that number < 8 | < 128
   while ((numCharacters < 8 || numCharacters > 128)) {
     alert("Invalid Entry" + "\nPassword must be at least 8 characters, and no more than 128 characters.")
     numCharacters = window.prompt("Choose a password length between 8 and 128 characters.");
   }
 
 
-  // // VALIDATE Lowercase letters and send array to passwordCredentials
-  // var lowercaseValid = confirm(lowercase);
 
-  // if (lowercaseValid === true) {
-  //   passwordCredentials.lowercase = alphaArray;
-  // }
+
+  // // VALIDATE Lowercase letters and send array to passwordCredentials
+  var lowercaseValid = confirm(lowercase);
 
   // // VALIDATE UPPERCASE letters and send array to passwordCredentials
-  // var uppercaseValid = confirm(uppercase);
-
-  // if (uppercaseValid === true) {
-  //   passwordCredentials.uppercase = alphaArrayUppercase;
-  // }
+  var uppercaseValid = confirm(uppercase);
 
   // // VALIDATE numbers and send array to passwordCredentials
-  // var numValid = confirm(numQuestion);
-
-  // if (numValid === true) {
-  //   passwordCredentials.numbers = numArray;
-  // }
+  var numValid = confirm(numQuestion);
 
   // // VALIDATE Special Characters and send array to passwordCredentials
-  // var specialValid = confirm(specialQuestion);
+  var specialValid = confirm(specialQuestion);
 
-  // if (specialValid === true) {
-  //   passwordCredentials.special = specialArray;
-  // }
+  if (lowercaseValid) {
+    passwordCredentials = passwordCredentials.concat(alphaArray);
+  }
 
-  // console.log(passwordCredentials);
+
+
+  if (uppercaseValid === true) {
+    passwordCredentials = passwordCredentials.concat(alphaArrayUppercase);
+  }
+
+
+
+  if (numValid === true) {
+    passwordCredentials = passwordCredentials.concat(numArray);
+  }
+
+
+  if (specialValid === true) {
+    passwordCredentials = passwordCredentials.concat(specialArray);
+  }
+
+  console.log(passwordCredentials);
+
+  var password = "";
+
+  for (let i = 0; i < numCharacters; i++) {
+    var randomNum = passwordCredentials[Math.floor(Math.random() * passwordCredentials.length)];
+    password = password + randomNum
+  }
+
+  return password;
+
 }
 
 
